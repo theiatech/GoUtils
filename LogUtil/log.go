ce10c4d0a3965aefc9e30aa0a9eb2c94b76dc3b3
theiatech <system@theiatech.cn>
2021-07-18 23:47:48 +0800

    Add LogDebug

--- LogUtil/log.go
+++ LogUtil/log.go
@@ -2,11 +2,16 @@ package LogUtil
 
 import (
 	"fmt"
+	"github.com/theiatech/GoUtils/FontColor"
 	"github.com/theiatech/GoUtils/TimeUtil"
 	"runtime"
 )
 
 func Log(str string)  {
+	fmt.Printf("\r\u001B[K%s %s",FontColor.Green(TimeUtil.Now()),str)
+}
+
+func LogDebug(str string)  {
 	_,file,line,_ := runtime.Caller(1)
-	fmt.Printf("\r\u001B[K%s [%s:%d] %s",TimeUtil.Now(),file,line,str)
+	Log(fmt.Sprintf("%s:%d %s",file,line,str))
 }
